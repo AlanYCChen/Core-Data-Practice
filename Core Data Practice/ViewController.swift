8dc2b0b05ce0570415f62c5e9181aa62bb195304
AlanChen <yuanching.chen.appledev@gmail.com>
2016-05-24 22:27:55 +0800

    Build core data practice

--- Core Data Practice/ViewController.swift
+++ Core Data Practice/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /* step 1: get app delegate & context */
+        
+        // setup a app delegate to connect with core data
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        // context is the handler let us access the database
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        /*
+        /* step 2: start to save data*/
+        let newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        
+         newUser.setValue("Ralph", forKey: "username")
+        newUser.setValue("pass456", forKey: "password")
+        
+        // save
+        do {
+            try context.save()
+        } catch {
+            print("error occurs in context saving process: save error")
+        }
+        */
+        
+        /* step 3: access the data*/
+        // request
+        let request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        
+        // search, constrain the selection of objects
+        request.predicate = NSPredicate(format: "username = %@ && password = %@", "Ralph" , "pass123")
+        
+        do {
+            let results = try context.executeFetchRequest(request)
+            
+            if results.count > 0 {
+                for result in results as! [NSManagedObject]{
+                    
+                    // delete object
+                    //context.deleteObject(result)
+                    
+                    // update/ adjust Tom's name
+                    //result.setValue("Ralph", forKey: "username")
+                    
+                    do { try context.save() } catch {}
+                    
+                    // print out the result
+                    if let username = result.valueForKey("username") as? String {
+                        print(username)
+                        //print(result.valueForKey("username")!)
+                    }
+                    if let password = result.valueForKey("password") as? String {
+                        print(password)
+                        //print(result.valueForKey("password")!)
+                    }
+                }
+            }
+        } catch {
+            print("context.executeFetchRequest failed")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
